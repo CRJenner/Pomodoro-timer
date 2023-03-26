97e5b0a73ac936a4083240844d644cabcbe45694
Chloe Jenner <crjdowning@hotmail.com>
2023-03-26 23:28:51 +0100

    progress bar added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import { useState } from 'react';
 import './App.css';
 import Settings from './components/Settings';
 import Timer from './components/Timer';
+import SettingContext from './Context/SettingContext';
 
 
 function App() {
   const [showSettings, setShowSettings]=useState(true)
+ 
   return (
     <div className="App">
       <main>
-        {showSettings ? <Settings/> : <Timer/>}
+        <SettingContext.Provider value={{
+meditationMinutes:5,
+        }}>
+           {showSettings ? <Settings/> : <Timer/>}
+        </SettingContext.Provider>
+       
       </main>
      
     </div>
--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,10 +1,20 @@
+import { useContext } from 'react'
 import ReactSlider from 'react-slider'
+import SettingContext from '../Context/SettingContext'
+
 function Settings(){
+    const settingTimer = useContext(SettingContext)
     return(
         <div style={{textAlign:'left'}}>
-            <label>Work minutes</label>
-            <ReactSlider/>
-            <label>break minutes</label>
+            <label>meditation {settingTimer.meditationMinutes}:00 minutes</label>
+            <ReactSlider
+            className= {'slider'}
+            thumbClassName={'thumb'}
+            trackClassName={'track'}
+            value={settingTimer.meditationMinutes}
+            min={1}
+            max={60}
+            />
         </div>
     )
 }
